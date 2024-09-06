c73167d4d44f772660f3356e7fbe95d4701abb57
Josef Janda <josefjanda@email.cz>
2024-09-06 16:32:22 +0200

    Repair 015_den_calendar

--- 015_deň/mesiac.java
+++ 015_deň/mesiac.java
@@ -1,55 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-public class mesiac {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class mesiac
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
 
-        // Požiada používateľa o zadanie čísla mesiaca
-        System.out.println("Zadajte číslo mesiaca (1-12): ");
-        int month = scanner.nextInt();
+		// Požiada používateľa o zadanie čísla mesiaca
+		System.out.println("Zadajte číslo mesiaca (1-12): ");
+		boolean validMonth = false;
 
-        // Switch príkaz na základe hodnoty mesiaca
-        switch (month) {
-            case 1:
-                System.out.println("Január");
-                break;
-            case 2:
-                System.out.println("Február");
-                break;
-            case 3:
-                System.out.println("Marec");
-                break;
-            case 4:
-                System.out.println("Apríl");
-                break;
-            case 5:
-                System.out.println("Máj");
-                break;
-            case 6:
-                System.out.println("Jún");
-                break;
-            case 7:
-                System.out.println("Júl");
-                break;
-            case 8:
-                System.out.println("August");
-                break;
-            case 9:
-                System.out.println("September");
-                break;
-            case 10:
-                System.out.println("Október");
-                break;
-            case 11:
-                System.out.println("November");
-                break;
-            case 12:
-                System.out.println("December");
-                break;
-            default:
-                System.out.println("Neplatné číslo mesiaca.");
-                break;
-        }
-        scanner.close();
-    }
+		while (!validMonth)
+		{
+			try
+			{
+				int month = Integer.parseInt(scanner.nextLine().trim());
+
+				String[] mesiace = { "Neplatné číslo měsíce.", // index 0, ale měsíce začínají od 1
+					"Leden", "Únor", "Březen", "Duben", "Květen", "Červen", "Červenec", "Srpen", "Září", "Říjen",
+					"Listopad", "Prosinec" };
+
+				if (month < 1 || month > 12)
+				{
+					System.out.println(mesiace[0]);
+				}
+				else
+				{
+					validMonth=true;
+					System.out.println(mesiace[month]);
+				}
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println("Prosím, zadejte platné číslo.");
+			}
+		}
+
+		scanner.close();
+	}
 }
